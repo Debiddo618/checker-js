dfa167cc43ce6385de09cb238f332a66a96bcde6
Debiddo <dzheng@terpmail.umd.edu>
2024-04-23 09:23:40 -0400

    created a function that checks if a move is inside the board

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const checkerBoardEl = document.querySelector(".checker-board");
 const message = document.querySelector("#message");
 
+let turn = -1;
+let selectedPiece;
+let moves = [];
+
 let checkerBoard = [
   [0, -1, 0, -1, 0, -1, 0, -1],
   [-1, 0, -1, 0, -1, 0, -1, 0],
@@ -33,18 +37,26 @@ function renderBoard(checkerBoard) {
       if (checkerBoard[i][j] === -1) {
         checkerCell.classList.add("occupied");
         checkerPiece.classList.add("red");
+        checkerCell.dataset.player = -1;
         checkerCell.append(checkerPiece);
       } else if (checkerBoard[i][j] === 0) {
         checkerCell.classList.add("unoccupied");
+        checkerCell.dataset.player = 0;
       } else {
         if (checkerBoard[i][j] === 1) {
           checkerCell.classList.add("occupied");
           checkerPiece.classList.add("white");
+          checkerCell.dataset.player = 1;
           checkerCell.append(checkerPiece);
         }
       }
-      checkerCell.id = `${i}` + `${j}`;
-      checkerPiece.id = `${i}` + `${j}`;
+      checkerCell.dataset.row = i;
+      checkerCell.dataset.col = j;
+
+      checkerPiece.dataset.row = i;
+      checkerPiece.dataset.col = j;
+      // checkerCell.id = `${i}` + `${j}`;
+      // checkerPiece.id = `${i}` + `${j}`;
 
       checkerCell.addEventListener("click", handleClick);
       checkerBoardEl.append(checkerCell);
@@ -55,5 +67,42 @@ function renderBoard(checkerBoard) {
 renderBoard(checkerBoard);
 
 function handleClick(event) {
-  console.log(event.target);
+  // console.log(event.target.dataset.row);
+  // console.log(event.target.dataset.col);
+  // console.log(event.target.dataset);
+  // console.warn(event.target);
+  findAvailiableMoves(event.target);
 }
+
+// takes in a piece and return availiable moves
+function findAvailiableMoves(piece) {
+  console.log(piece);
+  if (piece.dataset.player === "0") {
+    // console.log("It is a blank space");
+  } else if (piece.dataset.player === "1") {
+    console.log("white piece");
+  } else {
+    if (piece.dataset.player === "-1") {
+      // console.log("red piece");
+    }
+  }
+}
+
+// check if coordinate object is inbound
+function checkInBound(coordinate) {
+  // coordinate = {row,col}
+  if (
+    coordinate.row < 0 ||
+    coordinate.row > 7 ||
+    coordinate.col < 0 ||
+    coordinate.col > 7
+  ) {
+    return false;
+  }
+  return true;
+}
+
+
+// 1. click a cell
+// 2. click  another cell
+// 3. move the first cell to the second cell
